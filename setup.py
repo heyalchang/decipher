ec84c03458e3e26dc57e60e5a971505dfa3e969f
heyalchang <alchang@pobox.com>
2023-12-04 17:41:25 -0800

    Update setup.py
    update version

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='decipher',
-    url='https://github.com/dsymbol/decipher',
+    url='https://github.com/heyalchang/decipher',
     author='dsymbol',
     install_requires=[
-        'openai-whisper==20230314',
+        'openai-whisper==20231117',
         'tqdm'
     ],
     packages=find_packages(),
